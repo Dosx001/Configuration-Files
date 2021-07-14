5a9c419ade4e0a32f64ad763a13294188412df0a
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2021-07-14 15:47:56 -0700

    Edits

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
+ssh-keygen -t ed25519 -C "andresmichelrodriguez@gmail.com"
+ssh-add ~/.ssh/id_ed25519
+cat ~/.ssh/id_ed25519.pub
+read -p 'https://github.com/settings/ssh/new'
+
 sudo apt-get install git
-add-apt-repository ppa:git-core/ppa
-apt update
-apt install git
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt update
+sudo apt install git
 git config --global user.name "Andres Rodriguez"
 git config --global user.email "andresmichelrodriguez@gmail.com"
 git config --global core.editor vim
@@ -36,21 +41,22 @@ sudo make
 
 mkdir /mnt/c/download
 cd /mnt/c/download
-wget https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe
 wget https://justgetflux.com/flux-setup.exe
-wget https://phoenixnap.dl.sourceforge.net/project/qbittorrent/qbittorrent-win32/qbittorrent-4.3.6/qbittorrent_4.3.6_setup.exe
-wget https://mirrors.syringanetworks.net/videolan/vlc/3.0.16/win32/vlc-3.0.16-win32.exe
+wget https://github.com/ChrisAnd1998/TaskbarX/releases/download/1.7.0.0/TaskbarX_1.7.0.0_x64.zip
+wget https://github.com/White-Tiger/T-Clock/releases/download/v2.1.0%2357/T-Clock.zip
 wget https://cfhcable.dl.sourceforge.net/project/vcxsrv/vcxsrv/1.20.9.0/vcxsrv-64.1.20.9.0.installer.exe
 wget https://downloadmirror.intel.com/29183/eng/XTUSetup.exe
 wget https://wifimouse.necta.us/apk/MouseServer.exe
-wget https://dl.discordapp.net/distro/app/stable/win/x86/1.0.9002/DiscordSetup.exe
+wget https://mirrors.syringanetworks.net/videolan/vlc/3.0.16/win32/vlc-3.0.16-win32.exe
+wget https://phoenixnap.dl.sourceforge.net/project/qbittorrent/qbittorrent-win32/qbittorrent-4.3.6/qbittorrent_4.3.6_setup.exe
 wget https://az764295.vo.msecnd.net/stable/2d23c42a936db1c7b3b06f918cde29561cc47cd6/VSCodeUserSetup-x64-1.58.0.exe
-wget https://github.com/ChrisAnd1998/TaskbarX/releases/download/1.7.0.0/TaskbarX_1.7.0.0_x64.zip
-wget https://github.com/White-Tiger/T-Clock/releases/download/v2.1.0%2357/T-Clock.zip
 wget https://genshinimpact.mihoyo.com/client_app/launcher/20210609_faac20994b76230c/GenshinImpact_install_mihoyo_20210525204658.exe
+wget https://dl.discordapp.net/distro/app/stable/win/x86/1.0.9002/DiscordSetup.exe
+wget https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe
 wget https://www.gskill.com/gskill-device/keyboard/G.SKILL_RIPJAWSKeyboard_KM780_MX_Setup_V2.03.zip
+wget https://www.libreoffice.org/donate/dl/win-x86_64/7.0.6/en-US/LibreOffice_7.0.6_Win_x64.msi
 
-al origin
+cd /mnt/d/Repositories
 git clone https://github.com/sgolovine/nerdfont-patcher.git
 cd nerdfont-patcher
 sudo apt-get install software-properties-common
@@ -58,8 +64,4 @@ sudo add-apt-repository ppa:fontforge/fontforge
 sudo apt-get update
 sudo apt-get install fontforge
 fontforge -script font-patcher /mnt/c/Windows/Fonts/consola.ttf -c
-Explorer
-
-ssh-keygen -t ed25519 -C "andresmichelrodriguez@gmail.com"
-ssh-add ~/.ssh/id_ed25519
-cat ~/.ssh/id_ed25519.pub
+/mnt/c/Windows/explorer.exe .
