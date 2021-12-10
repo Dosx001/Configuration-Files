bdcde0e9a317a03c1af6c10b862a603d01d12a60
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2021-12-10 10:40:05 -0800

    Created update.sh

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cp ~/.vimrc vim
+cp -r ~/.vim/ftplugin ~/.vim/syntax ~/.vim/spell vim/.vim
+cp ~/.bashrc bash
+cp ~/.tmux.conf tmux
+cp ~/.zshrc zsh
+cp ~/.aliasme/cmd aliasme
+cp ~/rc.sh .
+cp ~/.gitconfig ~/.gitignore_global git
+cp /mnt/c/Users/Dosx001/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json windows_terminal
