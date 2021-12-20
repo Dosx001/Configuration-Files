68e20727df892782f4a964fe18aefa6734f864fb
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2021-12-20 08:55:26 -0800

    Added conditions to Arch boot script

--- setup/install.sh
+++ setup/install.sh
@@ -1,30 +1,61 @@
 #!/bin/bash
+# read -p 'Hostname: ' hostname
+# read -p 'Password: ' pass
+hostname="ArchTest"
+pass="pass"
 timedatectl set-ntp true
-efi="n\n\n\n+512M\nt\n1\n"
-swap="n\n\n\n+2G\nt\n\n19\n"
-root="n\n\n\n\nt\n\n24\n"
-echo -e "g\n$efi$swap${root}w\n" | fdisk /dev/sda
 
-mkfs.ext4 /dev/sda3
-mkswap /dev/sda2
-mkfs.fat -F32 /dev/sda1
+swap=$(free --giga | grep Mem | awk '{print $2}')
+if [[ $swap -lt 2 ]]; then
+    swap=$(($swap * 3))
+elif [[ $swap -lt 8 ]]; then
+    swap=$(($swap * 2))
+else
+    swap=$(($swap * 1.5))
+fi
+swap="+${swap}G"
 
-mount /dev/sda3 /mnt
-mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
-swapon /dev/sda2
+if [[ -d /sys/firmware/efi ]]; then
+    efi="n\n\n\n+512M\nt\n1\n"
+    swap="n\n\n\n${swap}\nt\n\n19\n"
+    root="n\n\n\n\nt\n\n24\n"
+    echo -e "g\n$efi$swap${root}w\n" | fdisk /dev/sda
 
-pacstrap /mnt base linux linux-firmware networkmanager intel-ucode
+    mkfs.ext4 /dev/sda3
+    mount /dev/sda3 /mnt
+    mkswap /dev/sda2
+    swapon /dev/sda2
+    mkfs.fat -F32 /dev/sda1
+    mkdir /mnt/boot
+    mount /dev/sda1 /mnt/boot
+else
+    swap="n\n\n\n\n${swap}\nt\n82\n"
+    root="n\n\n\n\n\n"
+    echo -e "$swap${root}w\n" | fdisk /dev/sda
+
+    mkfs.ext4 /dev/sda2
+    mount /dev/sda2 /mnt
+    mkswap /dev/sda1
+    swapon /dev/sda1
+fi
+
+if [[ $(cat /proc/cpuinfo | grep hypervisor) ]]; then
+    mirco=''
+elif [[ $(cat /proc/cpuinfo | grep name | awk '{print $4}') == "AMD" ]]; then
+    mirco='amd-ucode'
+else
+    mirco='intel-ucode'
+fi
+
+pacstrap /mnt base linux linux-firmware networkmanager $mirco
 genfstab -U /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt /bin/bash <<"EOT"
+arch-chroot /mnt /bin/bash <<EOT
 ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 hwclock --systohc
 
 sed -i "s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/" /etc/locale.gen
 locale-gen
-
-hostname="ArchTest"
 
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo $hostname > /etc/hostname
@@ -36,11 +67,9 @@ systemctl enable NetworkManager
 
 mkinitcpio -P
 
-# passwd
-
-bootctl --path=/boot install
+bootctl install
 sed -i "s/default.\+/default arch-*/" /boot/loader/loader.conf
-_UUID=$(blkid | grep '/dev/sda3' | awk '{print $2}' | sed 's/["|UUID=]//g')
+_UUID=$(blkid | grep /dev/sda3 | awk '{print $2}' | sed 's/["|UUID=]//g')
 echo "title Arch Linux
 linux /vmlinuz-linux
 initrd /intel-ucode.img
@@ -48,3 +77,5 @@ initrd /initramfs-linux.img
 options root=UUID=$_UUID rw" > /boot/loader/entries/arch.conf
 
 EOT
+(echo $pass; echo $pass) | passwd
+cat /boot/loader/entries/entries/arch.conf && cat /etc/hosts
