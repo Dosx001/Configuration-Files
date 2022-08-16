e9e61f2cea67baf76c5619534a574d682923e718
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2022-08-16 11:46:11 -0700

    Fixed lsp with LuaSnip error

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -196,6 +196,42 @@ require("telescope").setup({
 
 require("telescope").load_extension("fzf")
 
+local ls = require("luasnip")
+local types = require("luasnip.util.types")
+-- require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets/" })
+require("luasnip.loaders.from_vscode").lazy_load()
+
+ls.config.set_config({
+	history = true,
+	updateevents = "TextChanged,TextChangedI",
+	enable_autosnippets = true,
+	ext_opts = {
+		[types.choiceNode] = {
+			active = {
+				virt_text = { { " <- Current Choice", "NonTest" } },
+			},
+		},
+	},
+})
+
+vim.keymap.set({ "i", "s" }, "<A-j>", function()
+	if ls.expand_or_jumpable() then
+		ls.expand_or_jump()
+	end
+end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<A-k>", function()
+	if ls.jumpable(-1) then
+		ls.jump(-1)
+	end
+end, { silent = true })
+
+vim.keymap.set("i", "<A-l>", function()
+	if ls.choice_active() then
+		ls.change_choice(1)
+	end
+end)
+
 -- require("cmp_commit").setup({
 -- 	set = true,
 -- 	format = { "<<= ", " =>>" },
@@ -216,7 +252,6 @@ cmp.setup({
 	snippet = {
 		expand = function(args)
 			-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-			-- require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
 			ls.lsp_expand(args.body) -- For `luasnip` users.
 			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
 			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
@@ -504,42 +539,6 @@ Hydra.spawn = function(head)
 		dap_hydra:activate()
 	end
 end
-
-local ls = require("luasnip")
-local types = require("luasnip.util.types")
--- require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets/" })
-require("luasnip.loaders.from_vscode").lazy_load()
-
-ls.config.set_config({
-	history = true,
-	updateevents = "TextChanged,TextChangedI",
-	enable_autosnippets = true,
-	ext_opts = {
-		[types.choiceNode] = {
-			active = {
-				virt_text = { { " <- Current Choice", "NonTest" } },
-			},
-		},
-	},
-})
-
-vim.keymap.set({ "i", "s" }, "<A-j>", function()
-	if ls.expand_or_jumpable() then
-		ls.expand_or_jump()
-	end
-end, { silent = true })
-
-vim.keymap.set({ "i", "s" }, "<A-k>", function()
-	if ls.jumpable(-1) then
-		ls.jump(-1)
-	end
-end, { silent = true })
-
-vim.keymap.set("i", "<A-l>", function()
-	if ls.choice_active() then
-		ls.change_choice(1)
-	end
-end)
 
 require("colorizer").setup()
 require("Comment").setup()
