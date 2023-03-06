85195a676702180f0415d97f82e49165a7f72589
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-03-06 13:15:24 -0800

    Updated script

--- update.sh
+++ update.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-cp ~/.vimrc vim
-cp -r ~/.vim/ftplugin ~/.vim/syntax ~/.vim/spell vim/.vim
 cp ~/.bashrc bash
 cp ~/.tmux.conf tmux
 cp ~/.zshrc zsh
 cp ~/rc.sh .
 cp ~/.gitconfig ~/.gitignore_global git
-cp /mnt/c/Users/Dosx001/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json windows_terminal
-cp -r ~/.config/nvim .
+git submodule update --remote
+cp /mnt/c/Users/Dosx001/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json windows_terminal 2> /dev/null
