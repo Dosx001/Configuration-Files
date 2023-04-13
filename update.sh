50aa8e154b2d6e0dc347a857f37c3620bb311723
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-04-12 19:15:47 -0700

    update script

--- update.sh
+++ update.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-cp ~/.bashrc bash
 cp ~/.tmux.conf tmux
 cp ~/.zshrc zsh
-cp ~/rc.sh .
+cp ~/rc.sh home
 cp ~/.gitconfig ~/.gitignore_global git
 git submodule update --remote
 cp /mnt/c/Users/Dosx001/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json windows_terminal 2> /dev/null
